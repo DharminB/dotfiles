62e4064c2885d765e6d797432308b2e27c1f75eb
Dharmin <dharmingeo@gmail.com>
2020-11-06 11:18:08 +0100

    [i3] Revered back to using xbacklight for brightness control

--- scripts/change_brightness.sh
+++ scripts/change_brightness.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
-
-if [[ -f ~/.config/display_name && -f ~/.config/brightness ]]; then
-    echo "present"
-else
-    echo "Display name not known or brightness file missing"
-	echo "1.0" > ~/.config/brightness
-	xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1 > ~/.config/display_name
-    exit 1
-fi
-
-BRIGHTNESS="$(cat ~/.config/brightness)"
 if [ "$1" = "up" ]
 then
-    BRIGHTNESS=$(echo "$BRIGHTNESS+0.05" | bc)
-    if [ "$(echo "$BRIGHTNESS > 1.0" | bc)" -eq 1 ]; then
-        BRIGHTNESS='1.0'
-    fi
+    xbacklight -inc 2.5
 else
-    BRIGHTNESS=$(echo "$BRIGHTNESS-0.05" | bc)
-    if [ "$(echo "$BRIGHTNESS < 0.0" | bc)" -eq 1 ]; then
-        BRIGHTNESS='0.0'
-    fi
+    xbacklight -dec 2.5
 fi
-DISPLAY_NAME="$(cat ~/.config/display_name)"
-xrandr --output $DISPLAY_NAME --brightness $BRIGHTNESS
-echo $BRIGHTNESS > ~/.config/brightness
 
-BRIGHTNESS_PERC=$(echo "$BRIGHTNESS*100" | bc)
+brightness="$(xbacklight -get)"
 s="Brightness level:" 
-# notify-send -t 500 "$s" "$BRIGHTNESS_PERC"
-notify-send "$s" "$BRIGHTNESS_PERC" -h string:x-canonical-private-synchronous:anything
+echo $s
+#notify-send -u low "$s"
+notify-send "$s" "$brightness" -h string:x-canonical-private-synchronous:anything
