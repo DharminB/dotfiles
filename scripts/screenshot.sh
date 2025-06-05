ff0cedeeec44ded3c6c330b999500ed58c6dcb43
Dharmin <dharmingeo@gmail.com>
2025-06-05 12:38:34 +0200

    [scripts][screenshot.sh] Replaced : with - to work with sending imgs on teams

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$1" = "crop" ]
 then
-    scrot -s ~/Pictures/Screenshots/Screenshot_from_%F_%T_cropped.png
+    scrot -s ~/Pictures/Screenshots/Screenshot_from_%F_%H-%M-%S_cropped.png
 else
-    scrot ~/Pictures/Screenshots/Screenshot_from_%F_%T.png
+    scrot ~/Pictures/Screenshots/Screenshot_from_%F_%H-%M-%S.png
 fi
 
 notify-send -t 2000 "Screenshot taken" -h string:x-canonical-private-synchronous:anything
